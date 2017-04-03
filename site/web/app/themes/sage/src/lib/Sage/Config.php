03a405b365c0dee93f1d274da0d9a6d27f4cb88c
Simon Beasley <sie@medusamediacreations.co.uk>
2017-04-03 22:37:18 +0100

    Revert "add themes to gitignore"
    This reverts commit 7424cd34f6f1373dbc3908a57bf247c0078a4c83.

--- site/web/app/themes/sage/src/lib/Sage/Config.php
+++ site/web/app/themes/sage/src/lib/Sage/Config.php
@@ -1,5 +1,58 @@
 <?php
+
 namespace Roots\Sage;
-class Config extends \Illuminate\Config\Repository
+
+use \Illuminate\Config\Repository;
+use MedusaContentSuite\Config\Globals as McsGlobals;
+use MedusaContentSuite\CMB\Meta\PostMeta;
+use MedusaContentSuite\Helpers as Helpers;
+use MedusaContentSuite\Config\Menus;
+
+class Config extends Repository
 {
+    protected $postTypes = [];
+
+    public function __construct()
+    {
+        parent::__construct();
+        add_filter('wp', array($this, 'addClasses'));
+    }
+
+    public function addClasses(){
+        global $post;
+
+        $pt = get_post_type($post);
+
+        /*$tag = 'sage/template/post-type-archive-'.$pt.'/data';
+        add_filter( $tag, array($this, 'ptArchiveData' ) );*/
+
+
+    }
+
+
+
+    public function ptArchiveData( $data )
+    {
+        $pt = get_post_type();
+
+        //$data['teaser'] =
+
+        $data['image'] = $this->getMcsGlobals()->getArchiveTeaserImageSnippet();
+
+        //$data['pt_archive_title'] = $pt_archive_title;
+
+        return $data;
+    }
+
+
+    protected function getMcsGlobals()
+    {
+        return new McsGlobals;
+    }
+
+    protected function getHelpers()
+    {
+        return new Helpers;
+    }
+
 }
