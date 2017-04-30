4b24e42f5a1be1e014009d10964f126dad2a5d57
Simon Beasley <sie@medusamediacreations.co.uk>
2017-04-30 13:14:37 +0100

    add google site verification meta tag to head

--- site/web/app/themes/sage-wbba/templates/partials/head.blade.php
+++ site/web/app/themes/sage-wbba/templates/partials/head.blade.php
@@ -2,7 +2,7 @@
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="google-site-verification" content="xzA3WioBxfE_NBAZkZNeRJQzf4_rR1YcWgU-msaIOf4" />
+    <meta name="google-site-verification" content="Kq_3Rs1kpbpBDL24appto7_eDErGAiNQ1cT_Te5bxPA" />
   @php(wp_head())
 
     <style>
